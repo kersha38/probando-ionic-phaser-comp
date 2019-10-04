7820905c1eb4a5211a7a6432c8f8c9f520376eb4
carlos gutierrez <caralextan@hotmail.com>
2019-10-04 18:34:43 -0500

    creacion
    creacion e integracio ion-phaser

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+// phaser
+import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
+
+// phaser 
+defineIonPhaser(window);
